e7a8314533ad3f5becaa095645e1e55331f0d5ca
lelouc73 <loucasdurand@orange.fr>
2026-05-20 18:10:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import psycopg2
 
 app = FastAPI()
 
-DB_URL = "postgresql://postgres.tcwgywxgpzsdwcsofuvj:##Ne730380##@db.tcwgywxgpzsdwcsofuvj.supabase.co:6543/postgres?sslmode=require"
+DB_URL = "postgresql://postgres.tcwgywxgpzsdwcsofuvj:##Ne730380##@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
 
 @app.get("/")
 def test_connexion():

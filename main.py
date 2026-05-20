a008a485be28e00e49fc2647c2e7f98c952959a9
lelouc73 <loucasdurand@orange.fr>
2026-05-20 17:25:01 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import psycopg2
+
+app = FastAPI()
+
+# Remplace par ta vraie ligne Supabase (avec ton mot de passe)
+DB_URL = "postgresql://postgres##Ne730380##@db.tcwgywxgpzsdwcsofuvj.supabase.co:5432/postgres"
+
+@app.get("/")
+def test_connexion():
+    try:
+        # On essaie de se connecter à Supabase
+        conn = psycopg2.connect(DB_URL)
+        conn.close()
+        return {"status": "Succès", "message": "Connexion à Supabase réussie !"}
+    except Exception as e:
+        return {"status": "Erreur", "details": str(e)}
